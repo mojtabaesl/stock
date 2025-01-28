02dfd496467d0dc0ef1c8027eb455510bf143254
Mojtaba Eslami <mojtabaeslami.72@gmail.com>
2025-01-28 17:38:47 +0330

    take screenshot on exir login

--- src/login.ts
+++ src/login.ts
@@ -103,6 +103,7 @@ async function loginMofid(page: Page, username: string, password: string) {
 
 async function loginExir(page: Page, username: string, password: string) {
   await page.goto("https://boursebimeh.exirbroker.com/");
+  await page.screenshot({ path: "logs/00-login.jpg" });
   await page.waitForSelector("#userNameInput");
   await page.fill("#userNameInput", username);
   await page.fill("#mat-input-2", password);
